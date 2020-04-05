8c14529f7ced515f052aba8e83b551d2e3f3f3b4
Arvind Padmanabhan <arvindpdmn@gmail.com>
2020-04-05 13:44:32 +0530

    Update to runner.sh to handle spaces in base path. Tested on Google Drive access from Colab.

--- runner.sh
+++ runner.sh
@@ -13,14 +13,14 @@ iter=-1
 odir=$(date +%Y%h%d.%H%M%S)
 printf "======== Iteration %03d ========\n" $iter
 # Use maximum -m value to exhaust the entire seed file
-$PYPATH main.py -s -b $basepath -d $odir -m 1000
+$PYPATH main.py -s -b "$basepath" -d $odir -m 1000
 
-PendingFile=$basepath/$odir/pending_titles.txt
-while [ -s  $PendingFile ]
+PendingFile="$basepath"/$odir/pending_titles.txt
+while [ -s  "$PendingFile" ]
 do
     iter=$((iter+1))
     printf "======== Iteration %03d ========\n" $iter
-    lines=$(wc -l $PendingFile)
+    lines=$(wc -l "$PendingFile")
     echo Pending titles $lines...
-    $PYPATH main.py -r -b $basepath -d $odir -m 100
+    $PYPATH main.py -r -b "$basepath" -d $odir -m 100
 done
